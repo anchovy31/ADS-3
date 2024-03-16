2883181e26dc2ab7fe97ad2afc2d48bcf0f535f4
anchovy31 <146199120+anchovy31@users.noreply.github.com>
2024-03-16 20:09:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,12 +2,12 @@
 
 int cbinsearch(int* arr, int size, int value) {
   int k = 0, i = size / 2, beg = 0, left, right;
-  while (beg <= size) {
+  while (size > 0) {
     if (arr[i] == value) {
       k++;
       left = i - 1;
       right = i + 1;
-      while (arr[left] == value or arr[right] == value) {
+      while (arr[left] == value || arr[right] == value) {
         if (arr[left] == value) {
           k++;
           left--;
@@ -18,16 +18,14 @@ int cbinsearch(int* arr, int size, int value) {
         }
       }
       return k;
-    }
-    else if (value > arr[i]) {
+    } else if (value > arr[i]) {
       beg = i + 1;
       size -= beg;
       i = size / 2 + beg;
-    }
-    else {
-      beg = i + 1;
+    } else {
+      beg++; 
       size /= 2;
-      i = size / 2 + beg;
+      i = size / 2 + beg; 
     }
   }
   return 0;
