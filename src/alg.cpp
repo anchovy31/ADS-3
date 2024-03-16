2104911a4573fd50be62021e590957732e94e697
anchovy31 <146199120+anchovy31@users.noreply.github.com>
2024-03-16 19:40:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,34 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int cbinsearch(int* arr, int size, int value) {
+  int k = 0, i = size / 2, beg = 0, left, right;
+  while (beg <= size) {
+    if (arr[i] == value) {
+      k++;
+      left = i - 1;
+      right = i + 1;
+      while (arr[left] == value or arr[right] == value) {
+        if (arr[left] == value) {
+          k++;
+          left--;
+        }
+        if (arr[right] == value) {
+          k++;
+          right++;
+        }
+      }
+      return k;
+    }
+    else if (value > arr[i]) {
+      beg = i + 1;
+      size -= beg;
+      i = size / 2 + beg;
+    }
+    else {
+      beg = i + 1;
+      size /= 2;
+      i = size / 2 + beg;
+    }
+  }
+  return 0;
 }
